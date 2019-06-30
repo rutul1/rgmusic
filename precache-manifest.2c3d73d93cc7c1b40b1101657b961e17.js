5056eb23d41d8a217e73d4899969dc5bd1067d91
Rutul <rutul@creativeglance.com>
2019-06-30 15:59:58 +0530

    Updates

---
+++ precache-manifest.2c3d73d93cc7c1b40b1101657b961e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e56653eb6459a37f73acf64dc9fc922",
+    "url": "/rgmusic/index.html"
+  },
+  {
+    "revision": "0315010af487dce77d69",
+    "url": "/rgmusic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rgmusic/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0315010af487dce77d69",
+    "url": "/rgmusic/static/js/main.9121714d.chunk.js"
+  },
+  {
+    "revision": "6da32cb2900b378ed65f",
+    "url": "/rgmusic/static/js/runtime~main.1ecbe801.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rgmusic/static/media/logo.5d5d9eef.svg"
+  }
+]);
